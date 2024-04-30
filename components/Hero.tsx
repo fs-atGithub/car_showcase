ad119d42f90c03f4b29d453114cdc50c51c9549b
filip <filip.sostaric@protonmail.com>
2024-04-30 16:56:16 +0200

    Hero, Header and Footer is done

---
+++ components/Hero.tsx
@@ -0,0 +1,39 @@
+'use client';
+import React from 'react';
+import { CustomButton } from '@/components/';
+import Image from 'next/image';
+
+const Hero = () => {
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book,or rent a car quickly and easely!
+        </h1>
+        <p className="hero__subtitle">
+          We have the best cars in the city. Find the perfect car for your next
+          adventure.
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png"
+            alt="rent cars"
+            fill
+            className="object-contain"
+          />
+        </div>
+        <div className="hero__image-overlay"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
