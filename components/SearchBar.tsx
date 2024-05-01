fa7b48b9e54eadbb9b9b997a6341cfaadbb802c0
filip <filip.sostaric@protonmail.com>
2024-05-01 15:34:12 +0200

    SearchBar, Manufacturers

---
+++ components/SearchBar.tsx
@@ -0,0 +1,20 @@
+'use client';
+import React, { useState } from 'react';
+import { SearchManufacturer } from '@/components';
+
+const SearchBar = () => {
+  const [manufacturer, setManuFacturer] = useState('');
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManuFacturer={setManuFacturer}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
